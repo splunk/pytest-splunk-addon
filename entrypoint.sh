61a3d455e062f9aebdf54200db4b59a44ddbad53
rfaircloth-splunk <rfaircloth@splunk.com>
2020-01-29 21:47:04 -0500

    dockerize

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+echo check for splunk web
+wait-for $SPLUNK_HOST:8000 -t 0 -- echo splunkweb is up
+echo check for splunk mgmt
+wait-for $SPLUNK_HOST:8089 -t 0 -- echo splunkmgmt is up
+echo check for splunk hec
+wait-for $SPLUNK_HOST:8088 -t 0 -- echo splunkhec is up
+
+sleep 5
+
+echo check for splunk web
+wait-for $SPLUNK_HOST:8000 -t 0 -- echo splunkweb is up
+echo check for splunk mgmt
+wait-for $SPLUNK_HOST:8089 -t 0 -- echo splunkmgmt is up
+echo check for splunk hec
+wait-for $SPLUNK_HOST:8088 -t 0 -- echo splunkhec is up
+
+
+cd /work
+exec pytest tests $@
