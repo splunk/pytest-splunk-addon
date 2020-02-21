eb2bbc88f7900b8ab9c315becc0e0a8b6cd63ef2
rfaircloth-splunk <rfaircloth@splunk.com>
2020-02-21 09:18:49 -0500

    Update addon_basic.py

--- pytest_splunk_addon/standard_lib/addon_basic.py
+++ pytest_splunk_addon/standard_lib/addon_basic.py
@@ -4,19 +4,28 @@ import pytest
 from flaky import flaky
 
 
-class Basic():
+class Basic:
     logger = logging.getLogger()
+
+    @pytest.mark.splunk_addon_internal_errors
+    def test_splunk_internal_errors(self, splunk_search_util):
+        search = "search index=_internal CASE(ERROR) sourcetype!=splunkd_ui_access AND sourcetype!=splunk_web_access AND sourcetype!=splunk_web_service AND sourcetype!=splunkd_access AND sourcetype!=splunkd| dedup sourcetype| table sourcetype"
+
+        result = splunk_search_util.checkQueryCountIsGreaterThanZero(
+            search, interval=1, retries=1
+        )
+
+        assert result == False
 
     # This test ensures the contained samples will produce at lease one event per sourcetype
     @pytest.mark.splunk_addon_searchtime
-    @flaky(max_runs=5, min_passes=1)
     def test_basic_props(self, splunk_search_util, splunk_app_props):
         search = f"search (index=_internal OR index=*) AND {splunk_app_props['field']}=\"{splunk_app_props['value']}\""
 
         # run search
         result = splunk_search_util.checkQueryCountIsGreaterThanZero(
-            search,
-            interval=1, retries=1)
+            search, interval=1, retries=1
+        )
 
         if not result:
             return [
