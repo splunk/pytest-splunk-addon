ef444530cebb66d01fd51ddfc25b416473ea867d
Rinkal Domadiya (C) <rdomadiya@splunk.com>
2020-03-24 18:55:43 +0530

    ACD-3960: Implemented a test case scenario for eval field

--- pytest_splunk_addon/plugin.py
+++ pytest_splunk_addon/plugin.py
@@ -5,6 +5,8 @@ import os
 import pytest
 import requests
 from splunk_appinspect import App
+from pytest_splunk_addon.splunk import *
+from itertools import product
 
 from .helmut.manager.jobs import Jobs
 from .helmut.splunk.cloud import CloudSplunk
@@ -61,16 +63,24 @@ def return_props_sourcetype_param(id, value):
 
 
 def load_splunk_fields(props):
-    for p in props.sects:
-        section = props.sects[p]
+    for stanza_name in props.sects:
+        section = props.sects[stanza_name]
+        if section.name.startswith("source::"):
+            stanza_type = "source"
+            stanza_list = list(get_list_of_sources(stanza_name))
+        else:
+            stanza_type = "sourcetype"
+            stanza_list = [stanza_name]
         for current in section.options:
-            options = section.options[current]
-            if current.startswith("EXTRACT-"):
-                yield return_props_extract(p, options)
+            field_data = section.options[current]
+            for stanza_name in stanza_list:
+                if current.startswith("EXTRACT-"):
+                    yield return_props_extract(stanza_name, field_data, stanza_type)
+                elif current.startswith("EVAL-"):
+                    yield return_props_eval(stanza_name, field_data, stanza_type)
 
-
-def return_props_extract(id, value):
-    name = f"{id}_field::{value.name}"
+def return_props_extract(stanza_name, value, stanza_type):
+    name = f"{stanza_name}_field::{value.name}"
 
     regex = r"\(\?<([^\>]+)\>"
     matches = re.finditer(regex, value.value, re.MULTILINE)
@@ -81,4 +91,39 @@ def return_props_extract(id, value):
 
             fields.append(match.group(groupNum))
 
-    return pytest.param({"sourcetype": id, "fields": fields}, id=name)
+    return pytest.param({'stanza_type': stanza_type, "stanza_name": stanza_name, "fields": fields}, id=name)
+
+def return_props_eval(stanza_name, field_data, stanza_type):
+    '''
+    Return the fields parsed from EVAL as pytest parameters
+      
+    Args:
+        @stanza_name(str): source/sourcetype name
+        @field_data(object): Eval field details
+        @stanza_type: Stanza type (source/sourcetype)
+
+    Return:
+        List of pytest parameters
+    '''
+    name = f"{stanza_type}_{stanza_name}_field::{field_data.name}"
+    regex = r"EVAL-(?P<FIELD>.*)"
+    fields = re.findall(regex, field_data.name, re.IGNORECASE)
+
+    return pytest.param({'stanza_type': stanza_type, 'stanza_name': stanza_name, 'fields': fields}, id=name)
+
+def get_list_of_sources(source):
+    '''
+    Implement generator object of source list
+      
+    Args:
+        @param source(str): Source name
+    '''
+    match_obj = re.search(r"source::(.*)", source)
+    value = match_obj.group(1).replace("...", "*")
+    sub_groups = re.findall("\([^\)]+\)", value)
+    sub_group_list = []
+    for each_group in sub_groups:
+        sub_group_list.append(list(each_group.strip("()").split("|")))
+    template = re.sub(r'\([^\)]+\)', "{}",value)
+    for each_permutation in product(*sub_group_list):
+        yield template.format(*each_permutation)
