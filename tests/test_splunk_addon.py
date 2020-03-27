9f4af07cd920d49e86e920b36ad62a7a617ae223
Meet Shyani (C) <mshyani@splunk.com>
2020-03-27 17:41:27 +0530

    ACD-3837: Added one test case in the props.conf

--- tests/test_splunk_addon.py
+++ tests/test_splunk_addon.py
@@ -136,7 +136,7 @@ def test_splunk_app_fiction(testdir):
         "\n".join(result.stdout.lines),
         "\n".join(result.stderr.lines),
     )
-    result.assert_outcomes(passed=32, failed=0)
+    result.assert_outcomes(passed=35, failed=0)
 
     # make sure that that we get a '0' exit code for the testsuite
     assert result.ret == 0
