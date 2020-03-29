a4059a1bc84af1c12ae8246b067b672e7262f764
Jay Joshi <jay.joshi@crestdatasys.com>
2020-03-29 16:12:49 +0530

    ACD-3835: added field-alias test cases generation

--- tests/test_splunk_addon.py
+++ tests/test_splunk_addon.py
@@ -136,7 +136,7 @@ def test_splunk_app_fiction(testdir):
         "\n".join(result.stdout.lines),
         "\n".join(result.stderr.lines),
     )
-    result.assert_outcomes(passed=38, failed=0)
+    result.assert_outcomes(passed=68, failed=0)
 
     # make sure that that we get a '0' exit code for the testsuite
     assert result.ret == 0
@@ -184,7 +184,7 @@ def test_splunk_app_broken_sourcetype(testdir):
         "\n".join(result.stdout.lines),
         "\n".join(result.stderr.lines),
     )
-    result.assert_outcomes(passed=16, failed=5)
+    result.assert_outcomes(passed=13, failed=7)
 
     # The test suite should fail as this is a negative test
     assert result.ret != 0
