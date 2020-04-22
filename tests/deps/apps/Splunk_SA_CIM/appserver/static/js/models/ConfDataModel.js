b50316f22d5c0c4b09bb85f45fceacad96d91c8a
rfaircloth-splunk <rfaircloth@splunk.com>
2020-04-22 16:07:32 -0400

    opps

--- tests/deps/apps/Splunk_SA_CIM/appserver/static/js/models/ConfDataModel.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-define(['jquery', 'backbone', 'underscore', 'splunk.util', 'models/SplunkDBase'], function ($, Backbone, _, splunkUtils, SplunkDBaseModel) {
-    return SplunkDBaseModel.extend({
-        urlRoot: 'data/models'
-    });
-});
