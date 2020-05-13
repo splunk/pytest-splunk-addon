935e55660c743b4342783195c9b201fd2a423a28
Jay Joshi (C) <jjoshi@splunk.com>
2020-05-13 15:01:56 +0530

    ACD-4179: Added docker-compose fixture in conftest

--- tests/enable_saved_search_conftest.py
+++ tests/enable_saved_search_conftest.py
@@ -2,6 +2,32 @@ import pytest
 from splunklib import binding, client, results
 import time
 
+pytest_plugins = "pytester"
+
+def pytest_configure(config):
+    config.addinivalue_line("markers", "external: Test search time only")
+    config.addinivalue_line("markers", "docker: Test search time only")
+
+
+
+@pytest.fixture(scope="session")
+def docker_compose_files(request):
+    """
+    Get an absolute path to the  `docker-compose.yml` file. Override this
+    fixture in your tests if you need a custom location.
+
+    Returns:
+        string: the path of the `docker-compose.yml` file
+
+    """
+    docker_compose_path = os.path.join(
+        str(request.config.invocation_dir), "docker-compose.yml"
+    )
+    #LOGGER.info("docker-compose path: %s", docker_compose_path)
+
+    return [docker_compose_path]
+
+
 class TASetup(object):
     def __init__(self, splunk):
         self.splunk = splunk
