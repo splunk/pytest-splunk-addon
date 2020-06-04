f7880a110885827b57b50934114a62a31a743cab
Rinkal Domadiya (C) <rdomadiya@splunk.com>
2020-06-04 18:12:27 +0530

    ACD-4315 Added the sample_event file

---
+++ pytest_splunk_addon/standard_lib/sample_generation/sample_event.py
@@ -0,0 +1,7 @@
+class SampleEvent(object):
+    def __init__(self):
+        self.event = str 
+        self.key_fields = tuple
+        self.fields = list()
+        self.sourcetype = str 
+        self.source = str
