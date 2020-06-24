cca203b7ac3184178cb8bdac69a7787eb82d6eaf
Monish Shah <monshah@splunk.com>
2020-06-24 19:09:28 +0530

    ACD-4440:Removed-redundant-files

--- pytest_splunk_addon/standard_lib/test_main.py
+++
@@ -1,6 +0,0 @@
-from sample_generation import SampleGenerator
-
-sample_generator = SampleGenerator(r'G:\My Drive\TA-Factory\automation\testing\package')
-for each in (sample_generator.get_samples()):
-    print(each.event)
-    print(each.get_key_fields())
