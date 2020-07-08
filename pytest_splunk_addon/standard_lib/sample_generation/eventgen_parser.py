7975b63f0d2f87bd869a873696e919cd495c864d
Ryan Faircloth <35384120+rfaircloth-splunk@users.noreply.github.com>
2020-07-08 12:57:26 -0400

    Support samples location other than app path (#118)
    Check multiple locations for samples folders

--- pytest_splunk_addon/standard_lib/sample_generation/eventgen_parser.py
+++ pytest_splunk_addon/standard_lib/sample_generation/eventgen_parser.py
@@ -17,25 +17,46 @@ class EventgenParser:
     Args:
         addon_path (str): Path to the Splunk App 
     """
+
     splunk_test_type = " "
 
     def __init__(self, addon_path, config_path=None):
         self._app = App(addon_path, python_analyzer_enable=False)
-        self.config_path = config_path 
+        self.config_path = config_path
         self._eventgen = None
         self.addon_path = addon_path
-        self.path_to_samples = os.path.join(addon_path, "samples")
         self.match_stanzas = set()
+
+    @property
+    def path_to_samples(self):
+        if os.path.exists(os.path.join(self.config_path, "samples")):
+            return os.path.join(self.config_path, "samples")
+        elif os.path.exists(
+            os.path.join(
+                os.path.abspath(os.path.join(self.config_path, os.pardir)), "samples"
+            )
+        ):
+            return os.path.join(
+                os.path.abspath(os.path.join(self.config_path, os.pardir)), "samples"
+            )
+        else:
+            return os.path.join(self.addon_path, "samples")
 
     @property
     def eventgen(self):
         try:
             relative_path = os.path.relpath(self.config_path, self.addon_path)
-            if os.path.exists(os.path.join(self.config_path, "pytest-splunk-addon-data-generator.conf")):
-                self._eventgen = self._app.get_config("pytest-splunk-addon-data-generator.conf", dir=relative_path)
+            if os.path.exists(
+                os.path.join(
+                    self.config_path, "pytest-splunk-addon-data-generator.conf"
+                )
+            ):
+                self._eventgen = self._app.get_config(
+                    "pytest-splunk-addon-data-generator.conf", dir=relative_path
+                )
                 self.splunk_test_type = "splunk_indextime"
             else:
-                self._eventgen = self._app.get_config("eventgen.conf")    
+                self._eventgen = self._app.get_config("eventgen.conf")
                 self.splunk_test_type = "splunk_searchtime"
             return self._eventgen
         except OSError:
@@ -54,8 +75,7 @@ class EventgenParser:
         for sample_name, stanza_params in eventgen_dict.items():
             sample_path = os.path.join(self.path_to_samples, sample_name)
             yield SampleStanza(
-                sample_path,
-                stanza_params,
+                sample_path, stanza_params,
             )
 
     def get_eventgen_stanzas(self):
@@ -84,26 +104,33 @@ class EventgenParser:
             Dictionary representing eventgen.conf in the above format.
         """
         eventgen_dict = {}
-        child_dict = {'tokens': {}}
+        child_dict = {"tokens": {}}
         if os.path.exists(self.path_to_samples):
             for sample_file in os.listdir(self.path_to_samples):
                 for stanza in self.eventgen.sects:
                     if re.search(stanza, sample_file):
                         self.match_stanzas.add(stanza)
                         eventgen_sections = self.eventgen.sects[stanza]
-                        eventgen_dict.setdefault((sample_file), {
-                            'tokens': {}
-                        })
+                        eventgen_dict.setdefault((sample_file), {"tokens": {}})
                         for stanza_param in eventgen_sections.options:
                             eventgen_property = eventgen_sections.options[stanza_param]
-                            if eventgen_property.name.startswith('token'):
-                                _, token_id, token_param = eventgen_property.name.split('.')
+                            if eventgen_property.name.startswith("token"):
+                                _, token_id, token_param = eventgen_property.name.split(
+                                    "."
+                                )
                                 token_key = "{}_{}".format(stanza, token_id)
-                                if not token_key in eventgen_dict[sample_file]['tokens'].keys():
-                                    eventgen_dict[sample_file]['tokens'][token_key] = {}
-                                eventgen_dict[sample_file]['tokens'][token_key][token_param] = eventgen_property.value
+                                if (
+                                    not token_key
+                                    in eventgen_dict[sample_file]["tokens"].keys()
+                                ):
+                                    eventgen_dict[sample_file]["tokens"][token_key] = {}
+                                eventgen_dict[sample_file]["tokens"][token_key][
+                                    token_param
+                                ] = eventgen_property.value
                             else:
-                                eventgen_dict[sample_file][eventgen_property.name] = eventgen_property.value
+                                eventgen_dict[sample_file][
+                                    eventgen_property.name
+                                ] = eventgen_property.value
         return eventgen_dict
 
     def check_samples(self):
@@ -113,5 +140,11 @@ class EventgenParser:
         if os.path.exists(self.path_to_samples):
             for stanza in self.eventgen.sects:
                 if stanza not in self.match_stanzas:
-                    LOGGER.warning("No sample file found for stanza : {}".format(stanza))
-                    warnings.warn(UserWarning("No sample file found for stanza : {}".format(stanza)))
+                    LOGGER.warning(
+                        "No sample file found for stanza : {}".format(stanza)
+                    )
+                    warnings.warn(
+                        UserWarning(
+                            "No sample file found for stanza : {}".format(stanza)
+                        )
+                    )
