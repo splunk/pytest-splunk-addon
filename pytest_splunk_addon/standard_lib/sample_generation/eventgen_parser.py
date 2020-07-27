feb6e5f271866cdb96d8b6d0edc8480014dd21e8
Harsh Shah (C) <hashah@splunk.com>
2020-07-27 16:29:06 +0530

    BASE-11061: Update to to match stanza name from begining.

--- pytest_splunk_addon/standard_lib/sample_generation/eventgen_parser.py
+++ pytest_splunk_addon/standard_lib/sample_generation/eventgen_parser.py
@@ -122,7 +122,7 @@ class EventgenParser:
         if os.path.exists(self.path_to_samples):
             for sample_file in os.listdir(self.path_to_samples):
                 for stanza in self.eventgen.sects:
-                    if re.search(stanza, sample_file):
+                    if re.match(stanza, sample_file):
                         self.match_stanzas.add(stanza)
                         eventgen_sections = self.eventgen.sects[stanza]
                         eventgen_dict.setdefault((sample_file), {"tokens": {}})
