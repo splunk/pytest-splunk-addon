f243c1c0ae3564b3404ff5c8b27b5ff2ecdf82a5
monshah <monshah@splunk.com>
2020-07-29 15:31:07 +0530

    Added-syslog-input_type

--- pytest_splunk_addon/standard_lib/event_ingestors/ingestor_helper.py
+++ pytest_splunk_addon/standard_lib/event_ingestors/ingestor_helper.py
@@ -46,7 +46,7 @@ class IngestorHelper(object):
         ingestor_dict = dict()
         for event in tokenized_events:
             input_type = event.metadata.get("input_type")
-            if input_type in ["modinput", "windows_input"]:
+            if input_type in ["modinput", "windows_input", "syslog_tcp", "syslog_udp"]:
                 event.event = event.event.encode("utf-8").decode()
             else:
                 event.event = event.event.encode("utf-8")
