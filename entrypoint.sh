31255b1c867df462c71e53a928c890c2457fb466
rfaircloth-splunk <rfaircloth@splunk.com>
2020-09-25 17:53:09 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,7 @@
-#!/bin/sh
-echo check for splunk web
-wait-for $SPLUNK_HOST:8000 -t 0 -- echo splunkweb is up
-echo check for splunk mgmt
-wait-for $SPLUNK_HOST:8089 -t 0 -- echo splunkmgmt is up
-echo check for splunk hec
-wait-for $SPLUNK_HOST:8088 -t 0 -- echo splunkhec is up
-
-sleep 5
-
-echo check for splunk web
-wait-for $SPLUNK_HOST:8000 -t 0 -- echo splunkweb is up
-echo check for splunk mgmt
-wait-for $SPLUNK_HOST:8089 -t 0 -- echo splunkmgmt is up
-echo check for splunk hec
-wait-for $SPLUNK_HOST:8088 -t 0 -- echo splunkhec is up
-
+#!/bin/bash
 
 cd /work
 echo args $@
-sleep 10
+sleep 15
 source ~/.poetry/env
 exec poetry run pytest $@
