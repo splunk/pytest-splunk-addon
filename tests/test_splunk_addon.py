76c6b20545d96f6879b782e778be269ee9d71a98
rfaircloth-splunk <rfaircloth@splunk.com>
2020-09-28 17:18:00 -0400

    Update test_splunk_addon.py

--- tests/test_splunk_addon.py
+++ tests/test_splunk_addon.py
@@ -74,7 +74,6 @@ def test_splunk_connection_external(testdir):
         f"--splunk-host=splunk",
         f"--splunk-port=8089",
         "--splunk-forwarder-host=splunk",
-        "--splunk-forwarder-port=8088"
         "-v",
     )
 
