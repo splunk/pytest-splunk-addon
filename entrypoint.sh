369219f5d2727d830cc434d6a6fa5b6446a945f2
rfaircloth-splunk <rfaircloth@splunk.com>
2020-09-28 12:40:55 -0400

    updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,9 +19,14 @@ wait-for $SPLUNK_HOST:8088 -t 0 -- echo splunkhec is up
 cd /work
 echo args $@
 sleep 10
-export PATH="~/.pyenv/bin:$PATH"
-eval "$(pyenv init -)"          
+curl https://pyenv.run | bash ;\
+export PATH="~/.pyenv/bin:$PATH" ;\
+eval "$(pyenv init -)" ;\
+pyenv install 3.7.8 ;\
+pyenv local 3.7.8 ;\
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python 
 source ~/.poetry/env
+
 poetry install -E docker
 
 exec poetry run pytest $@
