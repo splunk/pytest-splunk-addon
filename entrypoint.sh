436be06eca0438453064090212a51754b015d209
Ryan Faircloth <35384120+rfaircloth-splunk@users.noreply.github.com>
2020-10-07 13:18:32 -0400

    Convert to Poetry try 2 (#187)
    * wip
    
    * Update test_matrix.yml
    
    * Update test_matrix.yml
    
    * Update test_matrix.yml
    
    * update lock
    
    * update
    
    * Update test_matrix.yml
    
    * Update test_matrix.yml
    
    * Update test_matrix.yml
    
    * Update test_matrix.yml
    
    * Update test_matrix.yml
    
    * updates
    
    * Update Dockerfile.tests
    
    * Update Dockerfile.tests
    
    * Update Dockerfile.tests
    
    * updates
    
    * Update Dockerfile.tests
    
    * update
    
    * Update test_splunk_addon.py
    
    * Update test_splunk_addon.py
    
    * Update test_splunk_addon.py
    
    * wip
    
    * Update entrypoint.sh
    
    * Update splunk.py
    
    * Update splunk.py
    
    * Update splunk.py
    
    * Update config.yml
    
    * build docs test
    
    * add theme for docs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,13 @@
-#!/bin/sh
-echo check for splunk web
-wait-for $SPLUNK_HOST:8000 -t 0 -- echo splunkweb is up
-echo check for splunk mgmt
-wait-for $SPLUNK_HOST:8089 -t 0 -- echo splunkmgmt is up
-echo check for splunk hec
-wait-for $SPLUNK_HOST:8088 -t 0 -- echo splunkhec is up
-
-sleep 5
-
-echo check for splunk web
-wait-for $SPLUNK_HOST:8000 -t 0 -- echo splunkweb is up
-echo check for splunk mgmt
-wait-for $SPLUNK_HOST:8089 -t 0 -- echo splunkmgmt is up
-echo check for splunk hec
-wait-for $SPLUNK_HOST:8088 -t 0 -- echo splunkhec is up
-
-
-cd /work
+#!/bin/bash
 echo args $@
-sleep 10
-exec pytest $@
+cd /work
+curl https://pyenv.run | bash ;\
+export PATH="~/.pyenv/bin:$PATH" ;\
+eval "$(pyenv init -)" ;\
+pyenv install 3.7.8 ;\
+pyenv local 3.7.8 ;\
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python 
+source ~/.poetry/env
+sleep 15
+poetry install -E docker
+exec poetry run pytest -vv $@
