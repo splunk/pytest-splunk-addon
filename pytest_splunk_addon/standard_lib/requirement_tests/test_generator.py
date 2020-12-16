1697c73f260d4676dc934d9c16995bc6f7ccb4d9
Nandini Vij <nvij@splunk.com>
2020-12-16 13:56:38 -0800

    Sample log file added

--- pytest_splunk_addon/standard_lib/requirement_tests/test_generator.py
+++ pytest_splunk_addon/standard_lib/requirement_tests/test_generator.py
@@ -1,7 +1,7 @@
 #
 # Inputs:   requirement file in TA's test folder
-# Function: Send event to test generator for testing
+# Function: Send event to test template for testing
 #           Parse XML   Extract Model to be verified from XML
 #                       Extract key value pair for (Field name and value from XML)
-#            Send one set of all above for each event in XM file and all the XML files in requirement folder
+#            Send one set of all above for each event in XML file and all the XML files in requirement folder
 
