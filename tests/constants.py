5cf166c6a3de139d78fd530a2503d1b349aff7b9
Raj Karkar <rkarkar@splunk.com>
2021-01-22 14:25:06 +0530

    test: Updated constant.py

--- tests/constants.py
+++ tests/constants.py
@@ -607,6 +607,15 @@ TA_FICTION_INDEXTIME_PASSED = [
     '*test_splunk_fiction_indextime.py::Test_App::test_indextime_time*test:indextime:test_pytest_splunk_addon_index_raw::test-index-samples-64* PASSED*',
     '*test_splunk_fiction_indextime.py::Test_App::test_indextime_line_breaker*test:indextime:test_pytest_splunk_addon_index_raw::test_index.samples* PASSED*',
     '*test_splunk_fiction_indextime.py::Test_App::test_indextime_line_breaker*test:indextime:test_index_HECEventIngestor::test_index_HECEventIngestor.samples* PASSED*',
+    '*test_splunk_fiction_indextime.py::Test_App::test_indextime_key_fields*test:indextime:uf_file_monitor_host_event::host-sample_host1_to_host-sample_host2* PASSED*',
+    '*test_splunk_fiction_indextime.py::Test_App::test_indextime_key_fields*test:indextime:uf_file_monitor_host_event::host-sample_host3_to_host-sample_host4* PASSED*',
+    '*test_splunk_fiction_indextime.py::Test_App::test_indextime_time*test:indextime:uf_file_monitor_host_plugin::uf-file-monitor-host-plugin-sample-3* PASSED*',
+    '*test_splunk_fiction_indextime.py::Test_App::test_indextime_time*test:indextime:uf_file_monitor_host_plugin::uf-file-monitor-host-plugin-sample-4* PASSED*',
+    '*test_splunk_fiction_indextime.py::Test_App::test_indextime_time*test:indextime:uf_file_monitor_rename_sourcetype:source::uf-file-monitor-rename-sourcetype-sample-5* PASSED*',
+    '*test_splunk_fiction_indextime.py::Test_App::test_indextime_time*test:indextime:uf_file_monitor_rename_sourcetype:source::uf-file-monitor-rename-sourcetype-sample-6* PASSED*',
+    '*test_splunk_fiction_indextime.py::Test_App::test_indextime_line_breaker*test:indextime:uf_file_monitor_host_event::uf_file_monitor_host_event.sample* PASSED*',
+    '*test_splunk_fiction_indextime.py::Test_App::test_indextime_line_breaker*test:indextime:uf_file_monitor_host_plugin::uf_file_monitor_host_plugin.sample* PASSED*',
+    '*test_splunk_fiction_indextime.py::Test_App::test_indextime_line_breaker*test:indextime:uf_file_monitor_rename_sourcetype:source::uf_file_monitor_rename_sourcetype.sample* PASSED*',
 ]
 
 """
