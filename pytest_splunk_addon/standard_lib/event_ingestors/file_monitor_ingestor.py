ba5d3fec6c338e1b658087076c619de53a2f45f9
Raj Karkar <rkarkar@splunk.com>
2021-01-24 23:43:11 +0530

    test: Added exception while creating file

--- pytest_splunk_addon/standard_lib/event_ingestors/file_monitor_ingestor.py
+++ pytest_splunk_addon/standard_lib/event_ingestors/file_monitor_ingestor.py
@@ -24,24 +24,28 @@ class FileMonitorEventIngestor(EventIngestor):
         self.create_output_conf()
         self.create_file_monitor_dir()
         for each_event in events:
-            self.create_file(each_event)
+            self.create_event_file(each_event)
             sleep(10)
             self.create_inputs_stanza(each_event)
 
     def create_output_conf(self):
         tcp_out_dict = {"name":"uf_monitor", "servers":"{}:{}".format(self.splunk_host, self.splunk_s2s_port)}
         response = requests.post(self.outputs_endpoint, tcp_out_dict, auth=(self.uf_username, self.uf_password), verify=False)
-        if response not in (200, 201):
-            print(response._content)
+        if response.status_code not in (200, 201):
+            LOGGER.error("Unable to add stanza in outputs.conf\nStatus code: {} \nReason: {} \ntext:{}".format(response.status_code, response.reason, response.text))
 
     def create_file_monitor_dir(self):
         monitor_dir = os.path.join(os.getcwd(), MONITOR_DIR)
         if not os.path.exists(monitor_dir):
             os.mkdir(monitor_dir)
+            
 
-    def create_file(self, event):
+    def create_event_file(self, event):
         with open(self.get_file_path(event), "w+") as fp:
-            fp.write(event.event)
+            try:
+                fp.write(event.event)
+            except PermissionError:
+                LOGGER.error("Unable to create event file for host : {}".format(event.metadata.get("host")))
 
     def create_inputs_stanza(self, event):
         file_path = self.get_file_path(event)
@@ -60,8 +64,8 @@ class FileMonitorEventIngestor(EventIngestor):
         if event.metadata.get("source"):
             stanza["rename-source"] = event.metadata.get("source")
         response = requests.post(self.inputs_endpoint, stanza, auth=(self.uf_username, self.uf_password), verify=False)
-        if response not in (200, 201):
-            print(response._content)
+        if response.status_code not in (200, 201):
+            LOGGER.error("Unable to add stanza in inputs.conf\nStatus code: {} \nReason: {} \ntext:{}".format(response.status_code, response.reason, response.text))
 
     def get_file_path(self, event):
         return "{}/{}/{}".format(os.getcwd(), MONITOR_DIR, event.metadata.get("host"))
