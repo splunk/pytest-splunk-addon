17607a16a0c094992570fff23366790a2fc9f1e5
kkania-splunk <76955023+kkania-splunk@users.noreply.github.com>
2021-02-01 12:55:09 +0100

    eventtype_parser unit tests (#251)
    * eventtype_parser unit tests
    
    * eventtype_parser no config file update
    
    * review remarks #1

---
+++ tests/unit/test_eventtype_parser.py
@@ -0,0 +1,26 @@
+def test_eventtypes_can_be_parsed_and_extracted(parser):
+    assert hasattr(
+        parser.eventtypes, "sects"
+    ), "eventypes can not be called or does have sects attribute"
+
+
+def test_eventtypes_can_be_parsed_and_returned(parser):
+    expected_outputs = [
+        {"stanza": x} for x in ["fake_splunkd", "fake_for_tags_positive"]
+    ]
+    for i, event in enumerate(parser.get_eventtypes()):
+        assert event == expected_outputs[i], "expeceted event {} not found".format(
+            expected_outputs[i]
+        )
+
+
+def test_get_eventtypes_calls_app_get_config(parser):
+    for _ in parser.get_eventtypes():
+        pass
+    parser.app.eventtypes_conf.assert_called_once()
+
+
+def test_no_eventtype_config_file(parser):
+    parser.app.eventtypes_conf.side_effect = OSError
+    output = [eventtype for eventtype in parser.get_eventtypes() if eventtype]
+    assert output == [], "eventtypes created when no config file exists"
