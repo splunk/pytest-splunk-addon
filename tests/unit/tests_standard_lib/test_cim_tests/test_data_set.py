539eacfe62b4bbfceacbdf42e270b7f30b129219
kkania-splunk <kkania@splunk.com>
2021-02-18 16:47:50 +0100

    test_data_model

--- tests/unit/tests_standard_lib/test_cim_tests/test_data_set.py
+++ tests/unit/tests_standard_lib/test_cim_tests/test_data_set.py
@@ -6,25 +6,6 @@ from pytest_splunk_addon.standard_lib.cim_tests.data_set import DataSet
 field = namedtuple("Field", ["name"])
 
 
-def test_dataset_can_be_loaded():
-    with patch.object(DataSet, "__init__", return_value=None) as data_set_mock:
-        list(
-            DataSet.load_dataset(
-                [
-                    {"name": "dataset1", "tags": ["tag1"]},
-                    {"name": "dataset2", "tags": ["tag2"]},
-                ],
-                "test",
-            )
-        )
-    data_set_mock.assert_has_calls(
-        [
-            call({"name": "dataset1", "tags": ["tag1"]}, "test"),
-            call({"name": "dataset2", "tags": ["tag2"]}, "test"),
-        ]
-    )
-
-
 @pytest.fixture()
 def mocked_dataset_constructor():
     from pytest_splunk_addon.standard_lib.cim_tests.data_set import DataSet, Field
@@ -53,6 +34,42 @@ def mocked_dataset_constructor():
         )
 
 
+@pytest.fixture()
+def dataset_mock():
+    with patch.object(DataSet, "__init__", return_value=None):
+        dataset = DataSet({}, "test_data_model")
+    dataset.name = "dataset2"
+    dataset.fields = [
+        field("bytes"),
+        field("bytes_in"),
+        field("bytes_out"),
+        field("dest"),
+        field("dest_ip"),
+        field("dest_mac"),
+    ]
+    dataset.tags = [["tag1", "tag1b"], ["tag2", "tag3"]]
+    return dataset
+
+
+def test_dataset_can_be_loaded():
+    with patch.object(DataSet, "__init__", return_value=None) as data_set_mock:
+        list(
+            DataSet.load_dataset(
+                [
+                    {"name": "dataset1", "tags": ["tag1"]},
+                    {"name": "dataset2", "tags": ["tag2"]},
+                ],
+                "test",
+            )
+        )
+    data_set_mock.assert_has_calls(
+        [
+            call({"name": "dataset1", "tags": ["tag1"]}, "test"),
+            call({"name": "dataset2", "tags": ["tag2"]}, "test"),
+        ]
+    )
+
+
 def test_dataset_instantiation(mocked_dataset_constructor):
     assert mocked_dataset_constructor.name == "dataset1"
     assert mocked_dataset_constructor.tags == ["test_tag1", "test_tag2"]
@@ -79,23 +96,6 @@ def test_dataset_string(mocked_dataset_constructor):
 
 def test_parse_constraints():
     assert DataSet._parse_constraint("constraints") == "constraints"
-
-
-@pytest.fixture()
-def dataset_mock():
-    with patch.object(DataSet, "__init__", return_value=None):
-        dataset = DataSet({}, "test_data_model")
-    dataset.name = "dataset2"
-    dataset.fields = [
-        field("bytes"),
-        field("bytes_in"),
-        field("bytes_out"),
-        field("dest"),
-        field("dest_ip"),
-        field("dest_mac"),
-    ]
-    dataset.tags = [["tag1", "tag1b"], ["tag2", "tag3"]]
-    return dataset
 
 
 def test_parse_fields_cluster(dataset_mock):
