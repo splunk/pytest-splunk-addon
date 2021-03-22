e3b8e3fbcf229f8a3f994f5a45df0682c199739d
kkania-splunk <kkania@splunk.com>
2021-03-22 17:09:40 +0100

    test_file_monitor_ingestor

--- tests/unit/tests_standard_lib/conftest.py
+++ tests/unit/tests_standard_lib/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from unittest.mock import mock_open
+from unittest.mock import mock_open, MagicMock
 
 
 @pytest.fixture()
@@ -7,3 +7,13 @@ def open_mock(monkeypatch):
     open_mock = mock_open()
     monkeypatch.setattr("builtins.open", open_mock)
     return open_mock
+
+
+@pytest.fixture()
+def mock_object(monkeypatch):
+    def create_mock_object(object_path):
+        mo = MagicMock()
+        monkeypatch.setattr(object_path, mo)
+        return mo
+
+    return create_mock_object
