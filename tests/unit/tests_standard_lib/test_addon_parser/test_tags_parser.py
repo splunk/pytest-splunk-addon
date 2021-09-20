7d6c7a6d918f0b71b59005cfe15c1c308cbdc4cc
Artem Rys <arys@splunk.com>
2021-09-20 21:12:44 +0200

    refactor: use conf-parser in TagsParser instead of splunk-appinspect

--- tests/unit/tests_standard_lib/test_addon_parser/test_tags_parser.py
+++ tests/unit/tests_standard_lib/test_addon_parser/test_tags_parser.py
@@ -1,31 +1,34 @@
+import os
+from unittest.mock import patch, mock_open
+
 import pytest
-from unittest.mock import patch, PropertyMock
+
 from pytest_splunk_addon.standard_lib.addon_parser.tags_parser import (
     TagsParser,
 )
 
-
-output_to_build = {
-    "eventtype=fiction_for_tags_positive": {
-        "tags_positive_event": "enabled",
-        "tags_disabled_event": "disabled",
-    },
-    "source=%2Fopt%2Fsplunk%2Fvar%2Flog%2Fsplunk%2Fsplunkd.log": {
-        "tags_positive_event": "enabled",
-        "tags_disabled_event": "disabled",
-    },
-}
+TEST_TAGS_WITHOUT_STANZA_NAME = """tags_positive_event = enabled
+tags_disabled_event = disabled
+"""
 
 
-def test_tags_can_be_parsed_and_extracted(parser_instance):
-    assert list(parser_instance.tags.sects.keys()) == [
-        "eventtype=fiction_for_tags_positive",
-        "source=%2Fopt%2Fsplunk%2Fvar%2Flog%2Fsplunk%2Fsplunkd.log",
-    ], "tags can not be called or does not have sects attribute"
+@pytest.fixture()
+def default_tags_parser():
+    return TagsParser(os.path.join(os.path.dirname(__file__), "testdata"))
 
 
-def test_tags_can_be_parsed_and_returned(parser_instance):
-    expected_outputs = [
+def test_tags_without_stanza_name():
+    expected = []
+    tags_parser = TagsParser("unused_path")
+    with patch(
+        "builtins.open", new_callable=mock_open, read_data=TEST_TAGS_WITHOUT_STANZA_NAME
+    ):
+        output = tags_parser.get_tags()
+        assert expected == list(output)
+
+
+def test_tags_can_be_parsed_and_returned(default_tags_parser):
+    expected = [
         {
             "stanza": 'eventtype="fiction_for_tags_positive"',
             "tag": "tags_positive_event",
@@ -47,36 +50,10 @@ def test_tags_can_be_parsed_and_returned(parser_instance):
             "enabled": False,
         },
     ]
-    for i, event in enumerate(parser_instance.get_tags()):
-        assert event == expected_outputs[i], "expeceted event {} not found".format(
-            expected_outputs[i]
-        )
+    result = default_tags_parser.get_tags()
+    assert expected == list(result)
 
 
-def test_get_tags_calls_app_get_config(parser_instance):
-    for _ in parser_instance.get_tags():
-        pass
-    parser_instance.app.get_config.assert_called_once_with("tags.conf")
-
-
-def test_no_tags_config_file(parser_instance):
-    parser_instance.app.get_config.side_effect = OSError
-    assert parser_instance.tags is None, "tags created when no config file exists"
-
-
-def test_nothing_returned_when_no_tags_config_file(parser):
-    with patch.object(TagsParser, "tags", new_callable=PropertyMock) as tags_mock:
-        tags_mock.return_value = None
-        parser_instance = parser(TagsParser, "get_config", {})
-        output = [tag for tag in parser_instance.get_tags() if tag]
-        assert output == [], "tags returned when no config file exists"
-
-
-@pytest.fixture(scope="module")
-def parsed_output(build_parsed_output):
-    return build_parsed_output(output_to_build)
-
-
-@pytest.fixture()
-def parser_instance(parsed_output, parser):
-    return parser(TagsParser, "get_config", parsed_output)
+def test_no_tags_config_file():
+    tags_parser = TagsParser("unused_path")
+    assert tags_parser.tags is None
