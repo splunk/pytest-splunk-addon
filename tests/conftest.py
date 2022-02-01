f91052505c9c06c2c5dcea10d2916b6c005a68d5
Dwijesh Shah <dwijeshs@splunk.com>
2022-02-01 18:14:57 +0530

    test: removed docker-compose related stanzas.

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,20 +9,3 @@ def pytest_configure(config):
     config.addinivalue_line("markers", "kubernetes: Test search time only")
     config.addinivalue_line("markers", "doc: Test Sphinx docs")
 
-
-@pytest.fixture(scope="session")
-def docker_compose_files(request):
-    """
-    Get an absolute path to the  `docker-compose.yml` file. Override this
-    fixture in your tests if you need a custom location.
-
-    Returns:
-        string: the path of the `docker-compose.yml` file
-
-    """
-    docker_compose_path = os.path.join(
-        str(request.config.invocation_dir), "docker-compose.yml"
-    )
-    # LOGGER.info("docker-compose path: %s", docker_compose_path)
-
-    return [docker_compose_path]
--- tests/test_splunk_addon.py
+++ tests/test_splunk_addon.py
@@ -60,26 +60,10 @@ def setup_test_dir(testdir):
         ),
         os.path.join(testdir.tmpdir, "tests/requirement_test_scripted"),
     )
-
-    # shutil.copy(
-    #     os.path.join(testdir.request.config.invocation_dir, "Dockerfile.splunk"),
-    #     testdir.tmpdir,
-    # )
-    # shutil.copy(
-    #     os.path.join(testdir.request.config.invocation_dir, "Dockerfile.uf"),
-    #     testdir.tmpdir,
-    # )
-    # shutil.copy(
-    #     os.path.join(testdir.request.config.invocation_dir, "Dockerfile.tests"),
-    #     testdir.tmpdir,
-    # )
-
-    # shutil.copy(
-    #     os.path.join(testdir.request.config.invocation_dir, "docker-compose.yml"),
-    #     testdir.tmpdir,
-    # )
     shutil.copytree(
-        os.path.join(testdir.request.config.invocation_dir, "tests/src"),
+        os.path.join(
+            testdir.request.config.invocation_dir, "tests/src"
+        ),
         os.path.join(testdir.tmpdir, "tests/src"),
     )
 
