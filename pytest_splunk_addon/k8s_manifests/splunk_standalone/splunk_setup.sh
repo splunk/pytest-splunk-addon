e440385cd5059ce82d1880f560ba531c0667c8cd
Dwijesh Shah <dwijeshs@splunk.com>
2022-02-23 21:20:09 +0530

    test: updatef splunk_setup.sh to install addon.

--- pytest_splunk_addon/k8s_manifests/splunk_standalone/splunk_setup.sh
+++ pytest_splunk_addon/k8s_manifests/splunk_standalone/splunk_setup.sh
@@ -5,12 +5,12 @@ kubectl wait pod nginx --for=condition=ready --timeout=300s -n $NAMESPACE_NAME
 cat "$(dirname -- "$0")"/addon_information.yaml > "$(dirname -- "$0")"/addon_information_updated.yaml
 export SC4S_INDEX_URL=$( curl -s https://api.github.com/repos/splunk/splunk-configurations-base-indexes/releases/latest \ | jq -r '.assets[] | select((.name | contains("spl")) and (.name | contains("search_head") | not) and (.name | contains("indexers") | not ) and (.name | contains("forwarders") | not)) | .browser_download_url ')
 echo $SC4S_INDEX_URL | sed "s|^|        - |" >> "$(dirname -- "$0")"/addon_information_updated.yaml
-if [ -f "$TEST_RUNNER_DIRECTORY/tests/src/$SPLUNK_ADDON.spl" ]; 
+if [ -f "$TEST_RUNNER_DIRECTORY/tests/src/$SPLUNK_ADDON" ]; 
 then
-    kubectl cp $TEST_RUNNER_DIRECTORY/tests/src/$SPLUNK_ADDON.spl nginx:/usr/share/nginx/html -n $NAMESPACE_NAME
-    echo "        - http://nginx//$SPLUNK_ADDON.spl" >> "$(dirname -- "$0")"/addon_information_updated.yaml;
+    kubectl cp $TEST_RUNNER_DIRECTORY/tests/src/$SPLUNK_ADDON nginx:/usr/share/nginx/html -n $NAMESPACE_NAME
+    echo "        - http://nginx//$SPLUNK_ADDON" >> "$(dirname -- "$0")"/addon_information_updated.yaml;
 else
-    echo "Unable to found $SPLUNK_ADDON.spl in tests/src"
+    echo "Unable to found $SPLUNK_ADDON in tests/src"
 fi
 grep -v ^\# "$(dirname -- "$0")"/addon_information_updated.yaml | grep . >> "$(dirname -- "$0")"/splunk_standalone_updated.yaml
 # kubectl create secret generic splunk-$NAMESPACE_NAME-secret --from-literal='password=Chang3d!' --from-literal='hec_token=9b741d03-43e9-4164-908b-e09102327d22' -n $NAMESPACE_NAME
