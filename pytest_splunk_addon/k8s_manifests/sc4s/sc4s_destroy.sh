3e12ccfc6ab3185396b6ff5c80824741181e2f99
Dwijesh Shah <dwijeshs@splunk.com>
2022-02-25 11:18:32 +0530

    test: remove setup and destroy shell scripts.

--- pytest_splunk_addon/k8s_manifests/sc4s/sc4s_destroy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-kubectl delete -f "$(dirname -- "$0")"/sc4s_deployment_updated.yaml -n $NAMESPACE_NAME
-sleep 15
-kubectl delete -f "$(dirname -- "$0")"/sc4s_service.yaml -n $NAMESPACE_NAME
