3c41023f7a462ca7525d634bfd7582e1e89cc471
Artem Rys <rysartem@gmail.com>
2022-05-25 13:11:28 +0200

    test: update Dockerfile.tests

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo args $@
 cd /work
-curl https://pyenv.run | bash ;\
-export PATH="~/.pyenv/bin:$PATH" ;\
-eval "$(pyenv init -)" ;\
-pyenv install 3.7.8 ;\
-pyenv local 3.7.8 ;\
+curl https://pyenv.run | bash
+export PATH="~/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+pyenv install 3.7.8
+pyenv local 3.7.8
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
 source ~/.poetry/env
 sleep 15
