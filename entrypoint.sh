3a4ab830fb42d9b82f0ca472c4935b1130007d07
mkolasinski-splunk <105011638+mkolasinski-splunk@users.noreply.github.com>
2022-08-31 20:26:49 +0200

    Update entrypoint.sh
    Co-authored-by: Artem Rys <rysartem@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ export PATH="~/.pyenv/bin:$PATH"
 eval "$(pyenv init -)"
 pyenv install 3.7.8
 pyenv local 3.7.8
-curl -sSL -sSL https://install.python-poetry.org | python
+curl -sSL https://install.python-poetry.org | python
 source ~/.poetry/env
 sleep 15
 poetry install -E docker
