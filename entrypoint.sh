a7309babde2ba39f9540da6db3a58d218a956b84
mkolasinski-splunk <105011638+mkolasinski-splunk@users.noreply.github.com>
2022-08-31 21:06:17 +0200

    chore: add Poetry to PATH

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ eval "$(pyenv init -)"
 pyenv install 3.7.8
 pyenv local 3.7.8
 curl -sSL https://install.python-poetry.org | python
+export PATH="/root/.local/bin:$PATH"
 source ~/.poetry/env
 sleep 15
 poetry install -E docker
