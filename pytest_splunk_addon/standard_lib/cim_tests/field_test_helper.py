d1884120426e9db609f43232f4caa1ace2b1a336
mkolasinski-splunk <105011638+mkolasinski-splunk@users.noreply.github.com>
2022-09-15 12:59:32 +0200

    Test/asserts improvements (#699)
    * chore: introduced logging to table in test_requirement_fields
    
    * chore: table logging introduced to test_datamodels
    
    * chore: improved logging
    
    * chore: unify assert logging and add log_helper module
    
    * chore: unit tests alignments
    
    * chore: improve error logging in fields_tests
    
    * chore: update black version on pre-commit
    
    * chore: black formatting

--- pytest_splunk_addon/standard_lib/cim_tests/field_test_helper.py
+++ pytest_splunk_addon/standard_lib/cim_tests/field_test_helper.py
@@ -20,6 +20,8 @@ Provides the helper methods to test addon_parser.Field object
 import logging
 import json
 from .field_test_adapter import FieldTestAdapater
+from ..utilities.log_helper import get_table_output
+from ..utilities.log_helper import format_search_query_log
 
 
 class FieldTestHelper(object):
@@ -190,7 +192,7 @@ class FieldTestHelper(object):
 
         """
         if not self.fields:
-            exc_message = self.get_table_output(
+            exc_message = get_table_output(
                 headers=["Source", "Sourcetype", "Event Count"],
                 value_list=[
                     [
@@ -202,7 +204,7 @@ class FieldTestHelper(object):
                 ],
             )
         elif len(self.fields) >= 1:
-            exc_message = self.get_table_output(
+            exc_message = get_table_output(
                 headers=[
                     "Source",
                     "Sourcetype",
@@ -232,36 +234,9 @@ class FieldTestHelper(object):
                     for each_result in self.parsed_result
                 ],
             )
-        exc_message += f"\n\nSearch = {self.search}"
+        exc_message += f"{format_search_query_log(self.search)}"
         for each_field in self.fields:
             exc_message += (
                 f"\n\nProperties for the field :: {each_field.get_properties()}"
             )
         return exc_message
-
-    @staticmethod
-    def get_table_output(headers, value_list):
-        """
-        Generate a table output of the following format::
-
-            Header1 | Header2
-            ---------------
-            One     | Value1
-            Two     | Value2
-            --------------
-
-        Args:
-            headers (list): list of headers
-            value_list (list of list): list of rows for the table
-        """
-        table_output = ""
-        table_list = [headers] + value_list
-        col_length = [
-            max(map(lambda cell: len(str(cell)), col)) for col in zip(*table_list)
-        ]
-        format_str = " | ".join(["{{:<{}}}".format(i) for i in col_length])
-        # Separating line
-        table_list.insert(1, ["-" * i for i in col_length])
-        for each_value in table_list:
-            table_output += format_str.format(*each_value) + "\n"
-        return table_output
