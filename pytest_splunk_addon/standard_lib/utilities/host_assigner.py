812394e89b59c8bd579848acd9c40e83dee84029
mkolasinski-splunk <mkolasinski@splunk.com>
2022-09-20 00:14:22 +0200

    chore: working version with ET

--- pytest_splunk_addon/standard_lib/utilities/host_assigner.py
+++ pytest_splunk_addon/standard_lib/utilities/host_assigner.py
@@ -14,12 +14,9 @@
 # limitations under the License.
 #
 import re
-import xmltodict
 import os
 import argparse
-from collections import defaultdict
-import xml.etree.ElementTree as ET
-
+from lxml import etree
 
 def assign_host(sample_path, pattern, output_dir=None):
     """
@@ -30,38 +27,32 @@ def assign_host(sample_path, pattern, output_dir=None):
         output_dir:   path to directory where separated files will be stored. Default the same dir as input file
         pattern:      regex pattern with capture group for host value in samples
     """
+
+    parser = etree.XMLParser(strip_cdata=False)
     if output_dir is None:
         output_dir = os.path.dirname(sample_path)
-    with open(sample_path, "r", encoding="utf-8") as sample_file:
-        sample_raw = sample_file.read()
-    samples = xmltodict.parse(sample_raw)
-    events = samples["device"]["event"]
-    events = events if type(events) == list else [events]
-    processed_events = list()
+    tree = etree.parse(sample_path, parser)
+    root = tree.getroot()
+    events = root.findall("event")
     filename, ext = os.path.splitext(sample_path)
     output_filename = f"{filename}_assigned_host{ext}"
     for each_event in events:
-        try:
-            extracted_hosts = re.search(pattern, each_event["raw"]).groups()
-            if len(extracted_hosts) == 0:
-                print(f"Using {pattern}. No host value matched.")
-            elif len(extracted_hosts) == 1:
-                host = extracted_hosts[0]
-                print(f"Found matching unique host value. Assigning host value to: {host}")
-                each_event["transport"]["@host"] = host
-                processed_events.append(each_event)
-            elif len(extracted_hosts) > 1:
-                found_hosts = [h for h in extracted_hosts]
-                print(f"Found multiple values matching pattern: {pattern}. Matching values: {found_hosts}")
+        raw = each_event.find("raw")
+        transport = each_event.find("transport")
+        if raw is not None and transport is not None:
+            try:
+                extracted_hosts = re.search(pattern, raw.text).groups()
+            except Exception as e:
+                print(f"Exception occured for event: {raw.text}")
+                print(e)
             else:
-                print("Undefined behavior")
-        except Exception as e:
-            print(f"Event: {each_event['raw']} caused an exception")
-            print(e)
-
-    samples["device"].update(event=processed_events)
-    with open(os.path.join(output_dir, output_filename), "w", encoding="utf-8") as output_file:
-        xmltodict.unparse(samples, output=output_file, pretty=True, indent="  ")
+                if len(extracted_hosts) == 1:
+                    host = extracted_hosts[0]
+                    print(f"Found matching unique host value. Assigning host value to: {host}")
+                    transport.set("host", host)
+                else:
+                    print(f"Unexpected behavior for pattern: {pattern} and sample {raw.text}")
+    tree.write(os.path.join(output_dir, output_filename), encoding="utf-8", xml_declaration=True)
 
 
 def main():
