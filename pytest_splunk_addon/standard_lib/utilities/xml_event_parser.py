4e2112a7a96e12576c54b38dc94a18d8f4ea56da
kkania-splunk <kkania@splunk.com>
2022-12-13 12:45:40 +0100

    fix: escape double quote

--- pytest_splunk_addon/standard_lib/utilities/xml_event_parser.py
+++ pytest_splunk_addon/standard_lib/utilities/xml_event_parser.py
@@ -100,6 +100,7 @@ def escape_char_event(event):
         "WHERE",
         "LIKE",
         "NOT",
+        '"',
     ]
     event = event.replace("\\", "\\\\")
     bounded_asterisk = re.search(r"\".*?\*+.*?\"", event)
