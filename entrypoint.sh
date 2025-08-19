5309da4053d45fe326b5826ddee774440cc4bed5
Marcin Kolasinski <mkolasinski@splunk.com>
2025-08-19 13:00:21 +0200

    chore: ci adjusmtents

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@ echo args $@
 cd /work
 curl -sSL https://install.python-poetry.org | python3.9 - --version 2.1.4
 export PATH="/root/.local/bin:$PATH"
-source ~/.poetry/env
 sleep 15
 poetry install
 exec poetry run pytest -vv $@
