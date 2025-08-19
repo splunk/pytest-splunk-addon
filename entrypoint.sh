1cd3707c28dc36d81a0609230c942fab07d025db
Marcin Kolasinski <mkolasinski@splunk.com>
2025-08-19 12:52:56 +0200

    chore: simplify entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 echo args $@
 cd /work
-curl https://pyenv.run | bash
-export PATH="~/.pyenv/bin:$PATH"
-eval "$(pyenv init -)"
-pyenv install 3.9.23
-pyenv local 3.9.23
-curl -sSL https://install.python-poetry.org | python - --version 2.1.4
+curl -sSL https://install.python-poetry.org | python3.9 - --version 2.1.4
 export PATH="/root/.local/bin:$PATH"
 source ~/.poetry/env
 sleep 15
