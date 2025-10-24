3863fb372bfaf4850552a9aeac31a5c65c641912
Harshil Gajera <hgajera@splunk.com>
2025-10-24 16:27:55 +0530

    ci: updating sc4s ingester

--- pytest_splunk_addon/event_ingestors/sc4s_event_ingestor.py
+++ pytest_splunk_addon/event_ingestors/sc4s_event_ingestor.py
@@ -1,93 +1,80 @@
-#
-# Copyright 2025 Splunk Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import socket
 from time import sleep
-import os
-import re
-import concurrent.futures
-from .base_event_ingestor import EventIngestor
 import logging
+from .base_event_ingestor import EventIngestor
 
 LOGGER = logging.getLogger("pytest-splunk-addon")
 
 
 class SC4SEventIngestor(EventIngestor):
     """
-    Class to Ingest Events via SC4S
-
-    The format for required_configs is::
-
-        {
-            sc4s_host (str): Address of the Splunk Server. Do not provide http scheme in the host.
-            sc4s_port (int): Port number of the above host address
-        }
-
-    Args:
-        required_configs (dict): Dictionary containing splunk host and sc4s port
+    Class to ingest events via SC4S (supports both IPv4 and IPv6)
     """
 
     def __init__(self, required_configs):
         self.sc4s_host = required_configs["sc4s_host"]
         self.sc4s_port = required_configs["sc4s_port"]
-        self.server_address = (
-            required_configs["sc4s_host"],
-            required_configs["sc4s_port"],
-        )
+
+    def _create_socket(self):
+        """Try all addresses (IPv4 and IPv6) and return a connected socket."""
+        last_exc = None
+        for res in socket.getaddrinfo(
+            self.sc4s_host, self.sc4s_port, socket.AF_UNSPEC, socket.SOCK_STREAM
+        ):
+            af, socktype, proto, canonname, sa = res
+            try:
+                sock = socket.socket(af, socktype, proto)
+                if af == socket.AF_INET6:
+                    # Attempt dual-stack if supported
+                    try:
+                        sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
+                    except (AttributeError, OSError):
+                        pass
+                sock.connect(sa)
+                return sock
+            except Exception as e:
+                last_exc = e
+                LOGGER.debug(f"Failed to connect to {sa}: {e}")
+                try:
+                    sock.close()
+                except Exception:
+                    pass
+                continue
+        raise ConnectionError(
+            f"Could not connect to SC4S at {self.sc4s_host}:{self.sc4s_port} via IPv4 or IPv6"
+        ) from last_exc
 
     def ingest(self, events, thread_count):
         """
-        Ingests events in the splunk via sc4s (Single/Batch of Events)
+        Ingests events in Splunk via SC4S (single/batch of events)
 
         Args:
             events (list): Events with newline character or LineBreaker as separator
-
         """
 
-        # This loop just checks for a viable remote connection
+        # Retry loop to establish connection
         tried = 0
-        sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-        sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
         while True:
             try:
-                sock.connect(self.server_address)
+                sock = self._create_socket()
                 break
             except Exception as e:
                 tried += 1
-                LOGGER.debug("Attempt {} to ingest data with SC4S".format(str(tried)))
+                LOGGER.debug(f"Attempt {tried} to ingest data with SC4S")
                 if tried > 90:
-                    LOGGER.error(
-                        "Failed to ingest event with SC4S {} times".format(str(tried))
-                    )
+                    LOGGER.error(f"Failed to ingest event with SC4S {tried} times")
                     raise e
                 sleep(1)
-            finally:
-                sock.close()
 
-        raw_events = list()
-        sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-        sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
-        sock.connect(self.server_address)
-        for event in events:
-            # raw_events.extend()
-            for se in event.event.splitlines():
-                try:
-                    sock.sendall(str.encode(se + "\n"))
-                except Exception as e:
-                    LOGGER.debug("Attempt ingest data with SC4S=".format(se))
-                    LOGGER.exception(e)
-                    sleep(1)
-        sock.close()
-
+        # Send events
+        try:
+            for event in events:
+                for se in event.event.splitlines():
+                    try:
+                        sock.sendall(str.encode(se + "\n"))
+                    except Exception as e:
+                        LOGGER.debug(f"Attempt ingest data with SC4S: {se}")
+                        LOGGER.exception(e)
+                        sleep(1)
+        finally:
+            sock.close()
